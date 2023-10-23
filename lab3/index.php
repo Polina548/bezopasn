e6ee3d35f856e3c121431ca97775ab1c5da39eee
Polina548 <130840241+Polina548@users.noreply.github.com>
2023-10-23 09:11:46 +0300

    Update index.php

--- lab3/index.php
+++ lab3/index.php
@@ -4,14 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title></title>
-</head>
-<body>
-<form method = "POST" action=" ">
-    <input placeholder = "Введите какой-то тег" type='text' Name='tag'>
-    <input type = "submit" value="запись">
-</form>
 <?php
-$tag= $_POST['tag'];
 include 'connect.php';
 $result = mysqli_query($link, 'SELECT tag FROM tags');
 while ($row = mysqli_fetch_array($result)) {
